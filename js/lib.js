6f88d4057d63aab31fa90f4949de578ba3a7d667
aspynlyn <lyn.aspyn@gmail.com>
2025-05-12 14:24:24 +0900

    [docs]:function

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,95 @@
+/**
+ * 숫자 더하기 기능
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}  덧셈 결과
+ */
+function add(a, b) {
+  return a + b;
+}
+
+/**
+ * 숫자 빼기 기능
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}  뺄셈 결과
+ */
+function minus(a, b) {
+  return a - b;
+}
+
+/**
+ * 숫자 곱하기 기능
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}  곱셈 결과
+ */
+function multi(a, b) {
+  return a * b;
+}
+
+/**
+ * 숫자 나누기 기능
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}  나눗셈 결과
+ */
+function divide(a, b) {
+  if (b === 0) {
+    return alert("분모는 0이 아니어야 합니다");
+  }
+  // 데이터 알아내고, 타입 비교하기
+  if (typeof a !== "number") {
+    return alert("분자는 숫자여야 합니다");
+  }
+  if (typeof b !== "number") {
+    return alert("분모는 숫자여야 합니다");
+  }
+  return a / b;
+}
+
+/**
+ * 계산기 기능
+ * 계산기 기능은 +, -, *, / 기능이 있습니다
+ * @param {string} symbol +, -, *, / 기호 중 1개 입력
+ * @param {number} a 숫자 입력
+ * @param {number} b 숫자 입력
+ * @returns {number} 결과는 숫자
+ *
+ * 사용예)
+ * ```javascript
+ * const result = calcurator("+", 5, 4)
+ */
+function calcurator(symbol, a) {
+  if (typeof symbol !== "string") {
+    return alert("기호를 입력하세요");
+  }
+  switch (symbol) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      minus(a, b);
+      break;
+    case "*":
+      multi(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+    default:
+      return alert("올바른 기호를 입력하세요");
+  }
+}
+
+// 외부로 공개할 함수 생성
+
+// 덧셈 사용
+const resultAdd = add(5, 4);
+const resultMinus = minus(5, 4);
+const resultMulti = multi(5, 4);
+const resultDevide = divide(5, 4);
+
 // for in 구문 예제 (대상은 객체 속성 반복)
 
 const singer = {
